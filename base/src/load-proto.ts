f76387895af93e0faf301266c54bc7428395bebb
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-05-19 15:23:15 -0300

    feat: proto file example

--- base/src/load-proto.ts
+++ base/src/load-proto.ts
@@ -1,10 +1,10 @@
 import path from 'path'
 import * as grpc from '@grpc/grpc-js'
 import * as protoLoder from '@grpc/proto-loader'
-// import { ChatHandlers } from './proto/chatPackage/Chat'
+// import { ChatHandlers } from './proto/package/Chat'
 // import { ProtoGrpcType } from './proto/chat'
-// import { TalkRequest } from './proto/chatPackage/TalkRequest'
-// import { TalkResponse } from './proto/chatPackage/TalkResponse'
+// import { TalkRequest } from './proto/package/TalkRequest'
+// import { TalkResponse } from './proto/package/TalkResponse'
 
 const HOST = '0.0.0.0:8082'
 const PROTO_FILE = path.resolve(__dirname, 'proto', 'chat.proto')
